901f7140ffc76a2994398cab641d7715dccef2b6
epignatelli <eduardo.pignatelli@burohappold.com>
2020-03-26 12:16:10 +0000

    installing python bhom package

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
-    name='Python_Tolkit',
+    name='The Buildings and Habitats object Model',
     description='A Machine Learning Framework for Visual Programming',
     author='Eduardo Pignatelli',
     author_email='info@bhom.xyz',
     license='LGPLv3',
-    packages=find_packages(),
-    version="3.1.0.0"
+    packages=["bh"],
+    version="3.1.0.0",
     )
