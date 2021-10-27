01795131036d44ddad295e98242464c2f7748c16
tg359 <tristan.gerrish@burohappold.com>
2021-10-27 09:14:02 +0100

    Fixed install process, need to fix removal process

--- Python_Engine/Compute/RemoveEnvironment.cs
+++ Python_Engine/Compute/RemoveEnvironment.cs
@@ -38,6 +38,7 @@ namespace BH.Engine.Python
         [Output("success", "True if environment successfully removed.")]
         public static bool RemoveEnvironment(this PythonEnvironment pythonEnvironment)
         {
+            // TODO - change file permissions to not be read-only - see SO post Cannot delete directory wit Directoyr.Deleter(path, true) post for details for recursive directory deletion!
             try
             {
                 Directory.Delete(Query.EnvironmentDirectory(pythonEnvironment), true);
