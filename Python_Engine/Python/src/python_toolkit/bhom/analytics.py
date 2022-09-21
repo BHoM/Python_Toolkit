ed73fb2947bb0270b3a850ce60b10327a24ffaf2
Tristan Gerrish <tristan.gerrish@burohappold.com>
2022-09-21 10:43:22 +0100

    updated analytics method

--- Python_Engine/Python/src/python_toolkit/bhom/analytics.py
+++ Python_Engine/Python/src/python_toolkit/bhom/analytics.py
@@ -2,7 +2,6 @@
 import inspect
 import sys
 import traceback
-from json import dump
 from pathlib import Path
 from typing import Callable
 from uuid import uuid4
@@ -14,6 +13,9 @@ from python_toolkit.bhom.version import version
 ASSEMBLIES_DIR = Path(r"C:\ProgramData\BHoM\Assemblies")
 LOGGING_DIR = Path(r"C:\ProgramData\BHoM\Logs")
 LOGGING_DIR.mkdir(exist_ok=True, parents=True)
+BHOM_VERSION = ".".join([str(i) for i in version((ASSEMBLIES_DIR / "BHoM.dll").as_posix())][:2])
+UI_NAME = "PythonBHoM"
+TICKS = ticks()
 
 
 def analytics(f: Callable):
@@ -21,19 +23,15 @@ def analytics(f: Callable):
 
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
-
-        bhom_version = ".".join([str(i) for i in version((ASSEMBLIES_DIR / "BHoM.dll").as_posix())][:2])
-        ui_name = "PythonBHoM"
-        ticks_value = ticks()
         
         _, module_stack, _, file_path = str(inspect.getmodule(f)).split(" ")
         module_stack = module_stack[1:-1]
 
-        log_file = LOGGING_DIR / f"Usage_{ui_name}_{ticks_value}.log"
+        log_file = LOGGING_DIR / f"Usage_{UI_NAME}_{TICKS}.log"
 
         # gather metadata around current callable instance
         d = {
-            "BHoMVersion": bhom_version,
+            "BHoMVersion": BHOM_VERSION,
             "BHoM_Guid": str(uuid4()),
             "CallerName": f"{module_stack}",
             "ComponentId": "",
@@ -47,17 +45,17 @@ def analytics(f: Callable):
             "SelectedItem":
             {
                 "Name": f.__name__,
-                "_bhomVersion": bhom_version,
+                "_bhomVersion": BHOM_VERSION,
                 "_t": "Python"
             },
             "Tags": [],
             "Time":
             {
-                "$date": ticks_value
+                "$date": TICKS
             },
-            "UI": ui_name,
+            "UI": UI_NAME,
             "UiVersion": sys.version,
-            "_bhomVersion": bhom_version,
+            "_bhomVersion": BHOM_VERSION,
             "_t": "BH.oM.UI.UsageLogEntry"
         }
 
@@ -67,11 +65,11 @@ def analytics(f: Callable):
         except Exception:
             d["Errors"].append(traceback.format_exc())
 
-        with open(log_file, "w+") as fp:
-            dump(d, fp, cls=Encoder)
+        with open(log_file, "a+") as fp:
+            fp.write(str(d) + "\n")
         
         # print function here to enable downstream processes to access the location where the data is stored for loading into BHoM.
-        print(log_file, file=sys.stdout)
+        #print(log_file, file=sys.stdout)
 
         return f(*args, **kwargs)
 
