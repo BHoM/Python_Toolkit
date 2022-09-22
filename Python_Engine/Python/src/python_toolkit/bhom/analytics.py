6ce2869faf5dae4e59bb12e5a1b0b8a2294bde84
Tristan Gerrish <tristan.gerrish@burohappold.com>
2022-09-22 09:37:10 +0100

    Changes made purely for a quick test to see if viable

--- Python_Engine/Python/src/python_toolkit/bhom/analytics.py
+++ Python_Engine/Python/src/python_toolkit/bhom/analytics.py
@@ -24,52 +24,52 @@ def analytics(f: Callable):
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
         
-        _, module_stack, _, file_path = str(inspect.getmodule(f)).split(" ")
-        module_stack = module_stack[1:-1]
+        #_, module_stack, _, file_path = str(inspect.getmodule(f)).split(" ")
+        #module_stack = module_stack[1:-1]
 
-        log_file = LOGGING_DIR / f"Usage_{UI_NAME}_{TICKS}.log"
+        #log_file = LOGGING_DIR / f"Usage_{UI_NAME}_{TICKS}.log"
 
-        # gather metadata around current callable instance
-        d = {
-            "BHoMVersion": BHOM_VERSION,
-            "BHoM_Guid": str(uuid4()),
-            "CallerName": f"{module_stack}",
-            "ComponentId": "",
-            "CustomData": {},
-            "Errors": [],
-            "FileId": "",
-            "FileName": "",
-            "Fragments": [],
-            "Name": "",
-            "ProjectID": "",
-            "SelectedItem":
-            {
-                "Name": f.__name__,
-                "_bhomVersion": BHOM_VERSION,
-                "_t": "Python"
-            },
-            "Tags": [],
-            "Time":
-            {
-                "$date": TICKS
-            },
-            "UI": UI_NAME,
-            "UiVersion": sys.version,
-            "_bhomVersion": BHOM_VERSION,
-            "_t": "BH.oM.UI.UsageLogEntry"
-        }
+        ## gather metadata around current callable instance
+        #d = {
+        #    "BHoMVersion": BHOM_VERSION,
+        #    "BHoM_Guid": str(uuid4()),
+        #    "CallerName": f"{module_stack}",
+        #    "ComponentId": "",
+        #    "CustomData": {},
+        #    "Errors": [],
+        #    "FileId": "",
+        #    "FileName": "",
+        #    "Fragments": [],
+        #    "Name": "",
+        #    "ProjectID": "",
+        #    "SelectedItem":
+        #    {
+        #        "Name": f.__name__,
+        #        "_bhomVersion": BHOM_VERSION,
+        #        "_t": "Python"
+        #    },
+        #    "Tags": [],
+        #    "Time":
+        #    {
+        #        "$date": TICKS
+        #    },
+        #    "UI": UI_NAME,
+        #    "UiVersion": sys.version,
+        #    "_bhomVersion": BHOM_VERSION,
+        #    "_t": "BH.oM.UI.UsageLogEntry"
+        #}
 
-        # run method and capture errors
-        try:
-            f(*args, **kwargs)
-        except Exception:
-            d["Errors"].append(traceback.format_exc())
+        ## run method and capture errors
+        #try:
+        #    f(*args, **kwargs)
+        #except Exception:
+        #    d["Errors"].append(traceback.format_exc())
 
-        with open(log_file, "a+") as fp:
-            fp.write(str(d) + "\n")
+        #with open(log_file, "a+") as fp:
+        #    fp.write(str(d) + "\n")
         
-        # print function here to enable downstream processes to access the location where the data is stored for loading into BHoM.
-        #print(log_file, file=sys.stdout)
+        ## print function here to enable downstream processes to access the location where the data is stored for loading into BHoM.
+        ##print(log_file, file=sys.stdout)
 
         return f(*args, **kwargs)
 
