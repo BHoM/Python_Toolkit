93a2476c273524d4d5f529f022f1d335293c2a95
BHoMBot <BHoMBot@burohappold.com>
2023-12-22 16:59:30 +0000

    Remove 7.0 Versioning

--- Python_Engine/Compute/BasePythonEnvironment.cs
+++ Python_Engine/Compute/BasePythonEnvironment.cs
@@ -37,7 +37,6 @@ namespace BH.Engine.Python
         [Input("reload", "Reload the base Python environment rather than recreating it, if it already exists.")]
         [Input("run", "Start the installation/retrieval of the BHoM Base Python Environment.")]
         [Output("env", "The base Python Environment for all BHoM workflows.")]
-        [PreviousVersion("7.0", "BH.Engine.Python.Compute.BasePythonEnvironment(System.Boolean)")]
         public static PythonEnvironment BasePythonEnvironment(
             bool reload = true,
             bool run = false
