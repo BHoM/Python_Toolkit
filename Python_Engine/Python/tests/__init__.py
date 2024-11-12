148923a6f75816eae1781b19f773c7a0171cdb80
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2024-11-12 15:43:40 +0000

    added python unit tests

---
+++ Python_Engine/Python/tests/__init__.py
@@ -0,0 +1,12 @@
+﻿from pathlib import Path
+import pandas as pd
+
+# identifier for all downstream processes
+BASE_IDENTIFIER = "PythonBHoM_pytest"
+
+def get_timeseries():
+    df = pd.read_csv(Path(__file__).parent / "assets" / "example_timeseries.csv", index_col="Timestamp")
+    df = df.set_index(pd.to_datetime(df.index))
+    return pd.Series(df["Value"], index=df.index)
+
+TIMESERIES_COLLECTION = get_timeseries()
