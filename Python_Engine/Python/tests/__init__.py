ebeda652d92abe10c1ee3f9fb1f49ab3bfa34d84
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2025-02-03 10:30:32 +0000

    use agg when testing plot methods

--- Python_Engine/Python/tests/__init__.py
+++ Python_Engine/Python/tests/__init__.py
@@ -1,5 +1,7 @@
 ﻿from pathlib import Path
 import pandas as pd
+import matplotlib as mpl
+
 
 # identifier for all downstream processes
 BASE_IDENTIFIER = "PythonBHoM_pytest"
@@ -10,3 +12,6 @@ def get_timeseries():
     return pd.Series(df["Value"], index=df.index)
 
 TIMESERIES_COLLECTION = get_timeseries()
+
+#use 'agg' for testing plot methods, as tkinter occasionally throws strange errors (missing component/library when component isn't missing) when the default backend is used only when using pytest
+mpl.use("agg")
