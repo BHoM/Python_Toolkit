f448e2f20279d5e0925336469c9ffca55c72ed42
Tom Kingstone <tom.kingstone@burohappold.com>
2026-05-28 11:13:02 +0100

    added extra class to handle non bhom objects serialised by the bhom serialiser, and improved from_json class method to use sub class initialiser to return the correct type when BHoMObject is sub classed.

--- Python_Engine/Python/src/python_toolkit/bhom/bhom_object.py
+++ Python_Engine/Python/src/python_toolkit/bhom/bhom_object.py
@@ -2,17 +2,17 @@ import uuid
 import re
 from typing import List, Dict
 import json
-from json import JSONEEncoder, JSONDecoder
+from json import JSONEncoder, JSONDecoder
 from .logging import CONSOLE_LOGGER
 
 def convert_pascal_to_camel(s: str):
-	"""Converts a string to camel_case."""
-	sections = re.split("(?<=.)(?=[A-Z])", s) #zero-length match before capitals, skipping capital at the 0th index
-	parts = []
-	for sec in sections:
-		parts.append(sec.lower())
+    """Converts a string to camel_case."""
+    sections = re.split("(?<=.)(?=[A-Z])", s) #zero-length match before capitals, skipping capital at the 0th index
+    parts = []
+    for sec in sections:
+        parts.append(sec.lower())
 
-	return parts.join("_")
+    return "_".join(parts)
 
 def convert_camel_to_pascal(s: str):
     """Converts a string to PascalCase, ignoring _ if it is the first character."""
@@ -20,102 +20,213 @@ def convert_camel_to_pascal(s: str):
 
     parts = []
     for sec in sections: #capitalise each section unless the section is empty (in which case, append an underscore) or the section starts with an underscore (first section can begin with _)
-		if sec == "":
-			parts.append("_")
-			continue
-		elif sec.startswith("_"):
-			parts.append(sec)
+        if sec == "":
+            parts.append("_")
+            continue
+        elif sec.startswith("_"):
+            parts.append(sec)
 
         parts.append(sec.capitalize())
 
     return  ''.join(parts)
 
 class BHoMJSONDecoder(JSONDecoder):
-	def __init__(self, *args, **kwargs):
-		super().__init__(self, object_hook=self.object_hook, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
 
-	def object_hook(self, d):
-		if "_t" not in d:
-			CONSOLE_LOGGER.debug(f"BHoMJSONDecoder could not convert the following dictionary into a BHoMObject due to a missing '_t' property. Falling back to dictionary: {d}")
-			return d
+    def object_hook(self, d):
+        if "_t" not in d:
+            CONSOLE_LOGGER.debug(f"BHoMJSONDecoder could not convert the following dictionary into a BHoMObject due to a missing '_t' property. Falling back to dictionary: {d}")
+            return d
+            
+        props = {
+            "_t": d.pop("_t"),
+            "_bhom_version": d.pop("_bhomVersion", None)
+        }
 
-		#get default BHoMObject properties and replace with defaults if not present
-		name = d.pop("Name", "")
-		bhom_guid = d.pop("BHoM_Guid", uuid.uuid4())
-		tags = d.pop("Tags", [])
-		fragments = d.pop("Fragments", {})
-		custom_data = d.pop("CustomData", {})
-		_t = d.pop("_t")
+        if d.get("BHoM_Guid", None) is not None:
+            #deserialise as BHoM Object
 
-		#convert all properties to camel_case as python users expect
-		props = {}
-		for prop_name in d:
-			props[convert_pascal_to_camel(prop_name)] = d[prop_name]
+            #get default BHoMObject properties and replace with defaults if not present
+            props["name"] = d.pop("Name", "")
+            props["bhom_guid"] = uuid.UUID(d.pop("BHoM_Guid"))
+            props["tags"] = d.pop("Tags", [])
+            props["fragments"] = d.pop("Fragments", [])
+            props["custom_data"] = d.pop("CustomData", {})
 
-		return BHoMObject(name, bhom_guid, tags, fragments, custom_data, _t, **props)
+            #convert all other properties to camel_case as python users expect
+            for prop_name in d:
+                props[convert_pascal_to_camel(prop_name)] = d[prop_name]
+
+            return BHoMObject(**props)
+        else:
+            #deserialise as IObject
+            for prop_name in d:
+                props[convert_pascal_to_camel(prop_name)] = d[prop_name]
+
+            return IObject(**props)
 
 class BHoMJSONEncoder(JSONEncoder):
-	def default(self, o):
-		if isinstance(o, BHoMObject):
-			#initialise special BHoMObject properties
-			props = {
-				"Name": o.name,
-				"BHoM_Guid": o.bhom_guid,
-				"Tags": o.tags,
-				"Fragments": o.fragments,
-				"CustomData": o.custom_data,
-				"_t": o._t
-			}
+    def default(self, o):
+        if isinstance(o, BHoMObject):
+            #initialise special BHoMObject properties
 
-			#get property names with reflection and convert all properties to PascalCase as the BHoM serialiser expects
-			for prop_name, value in vars(o).items():
-				if prop_name in ["name", "bhom_guid", "tags", "fragments", "custom_data", "_t"]
-					continue
-				props[convert_camel_to_pascal(prop_name)] = value
+            props = {
+                "Name": o.name,
+                "BHoM_Guid": str(o.bhom_guid),
+                "_t": o._t
+            }
 
-			return props
-			
-		return super(BHoMJSONDecoder, self).default(o) #fallback to default json decoder if object is not a BHoMObject (don't convert property case).
-	
-class BHoMObject:
-	name: str
-	bhom_guid: uuid.UUID
-	tags: List[str]
-	fragments: Dict[type, object]
-	custom_data: Dict[str, object]
-	_t: str
+            if len(o.tags) > 0:
+                props["Tags"] = o.tags
 
-	def __init__(
-		self,
-		name: str = "",
-		bhom_guid: uuid.UUID = uuid.uuid4(),
-		tags: List[str] = [],
-		fragments: Dict[type, object] = {},
-		custom_data: Dict[str, object] = {},
-		_t: str = "BH.oM.Base.BHoMObject"
-		**kwargs
-	) -> BHoMObject:
+            if len(o.fragments) > 0:
+                props["Fragments"] = o.fragments
 
-		self.name = name
-		self.bhom_guid = bhom_guid
-		self.fragments = fragments
-		self.tags = tags
-		self.custom_data = custom_data
-		self._t = _t
+            if len(o.custom_data) > 0:
+                props["CustomData"] = o.custom_data
 
-		#set non-CustomData properties with reflection.
-		for kwarg in kwargs:
-			setattr(self, kwarg, kwargs[kwarg])
+            if o._bhom_version is not None:
+                props["_bhomVersion"] = o._bhom_version
 
-	@classmethod
-	def from_json(j: str) -> 'BHoMObject':
-		obj = json.loads(json, decoder=BHoMJSONDecoder)
+            #get property names with reflection and convert all properties to PascalCase as the BHoM serialiser expects
+            for prop_name, value in vars(o).items():
+                if prop_name in ["name", "bhom_guid", "tags", "fragments", "custom_data", "_t", "_bhom_version"]:
+                    continue
 
-		if not isinstance(obj, BHoMObject): #this only tests that the top level object was deserialised correctly, if there are problems with deep properties, change the CONSOLE_LOGGER log level to debug.
-			raise TypeError("The object provided does not deserialise to a valid BHoM object.")
+                props[convert_camel_to_pascal(prop_name)] = value
 
-		return obj
-	
-	def to_json(self) -> str:
-		s = json.dumps(self, encoder=BHoMJSONEncoder)
-		return s
+            return props
+        elif isinstance(o, IObject):
+            props = {
+                "_t": o._t
+            }
+
+            if o._bhom_version is not None:
+                props["_bhomVersion"] = o._bhom_version
+
+            for prop_name, value in vars(o).items():
+                if prop_name in ["_t", "_bhom_version"]:
+                    continue
+
+                props[convert_camel_to_pascal(prop_name)] = value
+
+            return props
+        elif isinstance(o, uuid.UUID): #UUID object is not json serialisable by default
+            return str(o)
+        
+        return super(type(self), self).default(o) #fallback to default json decoder if object is not a BHoMObject (don't convert property case).
+    
+class IObject:
+    """More generic version of BHoMObject, for non-native objects serialised by the BHoM serialiser, but do not inherit from BHoMObject."""
+    _t: str
+    _bhom_version: str
+
+    def __init__(
+        self,
+        _t: str,
+        _bhom_version: str = None,
+        **kwargs
+    ) -> 'IObject':
+        self._t = _t
+        self._bhom_version = _bhom_version
+        
+        #set properties with reflection.
+        for kwarg in kwargs:
+            setattr(self, kwarg, kwargs[kwarg])
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__} of type {self._t}, version: '{getattr(self, "_bhom_version", "Unknown")}'"
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, IObject):
+            return False
+
+        if self._t != other._t:
+            return False
+            
+        vself = vars(self).copy()
+        vother = vars(other).copy()
+
+        #ignore these properties when comparing by property.
+        ignore = ["_bhom_version"]
+        _ = [(vself.pop(p, None), vother.pop(p, None)) for p in ignore]
+
+        return vself == vother
+
+    @classmethod
+    def from_json(cls, j: str) -> 'IObject':
+        obj = json.loads(j, cls=BHoMJSONDecoder)
+
+        if not isinstance(obj, cls): #this only tests that the top level object was deserialised correctly, if there are problems with deep properties, change the CONSOLE_LOGGER log level to debug.
+            raise TypeError("The object provided does not deserialise to a valid BHoM object.")
+
+        return obj
+    
+    def to_json(self) -> str:
+        return json.dumps(self, cls=BHoMJSONEncoder)
+
+class BHoMObject(IObject):
+    name: str
+    bhom_guid: uuid.UUID
+    tags: List[str]
+    fragments: List[Dict[str, object]]
+    custom_data: Dict[str, object]
+
+    def __init__(
+        self,
+        _t: str, #don't make default, as subclasses of this class should set this with super().__init__
+        name: str = "",
+        bhom_guid: uuid.UUID = uuid.uuid4(),
+        tags: List[str] = [],
+        fragments: List[Dict[str, object]] = [],
+        custom_data: Dict[str, object] = {},
+        _bhom_version: str = None,
+        **kwargs
+    ) -> 'BHoMObject':
+        self._t = _t
+        self.name = name
+        self.bhom_guid = bhom_guid
+        self.fragments = fragments
+        self.tags = tags
+        self.custom_data = custom_data
+        self._bhom_version = _bhom_version
+
+        #set non-CustomData properties with reflection.
+        for kwarg in kwargs:
+            setattr(self, kwarg, kwargs[kwarg])
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__} of type {self._t}, name: '{self.name}', version: '{getattr(self, "_bhom_version", "Unknown")}', id: '{self.bhom_guid}'"
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, BHoMObject):
+            return False
+
+        if self._t != other._t:
+            return False
+
+        vself = vars(self).copy()
+        vother = vars(other).copy()
+
+        #ignore these properties when comparing by property.
+        ignore = ["bhom_guid", "_bhom_version"]
+        _ = [(vself.pop(p, None), vother.pop(p, None)) for p in ignore]
+
+        return vself == vother
+
+    @classmethod
+    def from_json(cls, j: str):
+        obj = json.loads(j, cls=BHoMJSONDecoder)
+
+        if issubclass(cls, BHoMObject) and cls != BHoMObject:
+            obj = cls._from_bhom_object(obj)
+
+        if not isinstance(obj, cls): #this only tests that the top level object was deserialised correctly, if there are problems with deep properties, change the CONSOLE_LOGGER log level to debug.
+            raise TypeError("The object provided does not deserialise to a valid BHoM object.")
+
+        return obj
+    
+    @classmethod
+    def _from_bhom_object(cls, o: 'BHoMObject'):
+        return cls(**vars(o).copy()) #assuming that the sub class is correctly set up, then this should work
--- Python_Engine/Python/tests/test_bhom_serialiser.py
+++ Python_Engine/Python/tests/test_bhom_serialiser.py
@@ -1,11 +1,13 @@
 from python_toolkit.bhom.bhom_object import convert_pascal_to_camel, convert_camel_to_pascal, BHoMObject, BHoMJSONDecoder, BHoMJSONEncoder
+import uuid
 import json
 
-SERIALISED_BHOM_OBJECT = '{"_t": "BH.oM.Base.BHoMObject", "Name": "test_object"}' #TODO: use the BHoM serialiser to make a BHoMObject json string, and use that here.
-DESERIALISED_BHOM_OBJECT = BHoMObject(name = "test_object") #TODO: make equivalent BHoMObject here identical to the one above.
+SERIALISED_BHOM_OBJECT = '{"_t": "BH.oM.Base.BHoMObject", "Name": "test_object", "BHoM_Guid": "91ec5ba6-88cd-4b3c-b12a-3d88f0252cde"}' #TODO: use the BHoM serialiser to make a BHoMObject json string, and use that here.
+DESERIALISED_BHOM_OBJECT = BHoMObject(_t = "BH.oM.Base.BHoMObject", bhom_guid=uuid.UUID("91ec5ba6-88cd-4b3c-b12a-3d88f0252cde"), name = "test_object") #TODO: make equivalent BHoMObject here identical to the one above.
 
 def test_case_convert():
 	"""Test that the camel and pascal converters are working correctly by using expected outputs and a round trip both ways."""
+	#TODO: find edge cases within bhom to see if round trip converters work properly.
 
 	#arrange
 	test_pascal_str = "ThisIsAPascalCaseString"
@@ -26,22 +28,43 @@ def test_case_convert():
 	assert pascal_round_trip == test_pascal_str, f"pascal round trip got '{pascal_round_trip}' but expected '{test_pascal_str}'."
 	assert camel_round_trip == test_camel_str, f"camel round trip got '{camel_round_trip}' but expected '{test_camel_str}'."
 
-def test_serialise_bhom_object()
+def test_serialise_bhom_object():
 	"""Test that bhom objects serialise correctly to a format that the c# bhom serialiser accepts as valid, and with the correct property case."""
 
-	serialised = json.dumps(DESERIALISED_BHOM_OBJECT, encoder=BHoMJSONEncoder)
+	serialised = json.dumps(DESERIALISED_BHOM_OBJECT, cls=BHoMJSONEncoder)
 	serialised_bhom_object_to_json = DESERIALISED_BHOM_OBJECT.to_json()
 	round_trip = BHoMObject.from_json(serialised)
 
 	assert serialised == serialised_bhom_object_to_json, f"Direct serialisation to json differed to BHoMObject to_json method."
 	assert round_trip == DESERIALISED_BHOM_OBJECT, f"BHoMObject round trip failed for serialisation -> deserialisation." #this is the only direction the round trip can be tested without directly inspecting each dictionary entry, as it is not guaranteed that the other direction will produce identical order for json strings.
 
-def test_deserialise_bhom_object()
+def test_deserialise_bhom_object():
 	"""Test that bhom objects deserialise correctly with no errors with expected properties with correct case."""
 
-	obj = json.loads(SERIALISED_BHOM_OBJECT, decoder=BHoMJSONDecoder)
+	obj = json.loads(SERIALISED_BHOM_OBJECT, cls=BHoMJSONDecoder)
 	obj_bhom_object_from_json = BHoMObject.from_json(SERIALISED_BHOM_OBJECT)
 
 	assert isinstance(obj, BHoMObject), "JSON decoded an object of the wrong type"
 	assert obj == DESERIALISED_BHOM_OBJECT, f"Actual deserialised object ({obj}) was not identical to expected deserialised object ({DESERIALISED_BHOM_OBJECT})."
 	assert obj_bhom_object_from_json == obj, f"Direct deserialisation from json differed to BHoMObject from_json method."
+
+def test_subclass():
+	
+	class TestSubClass(BHoMObject):
+		_t: str = "BH.oM.Base.CustomObject"
+		some_other_data: str
+
+		def __init__(self, some_other_data, **kwargs):
+			self.some_other_data = some_other_data
+			_t = kwargs.pop("_t", self._t)
+			super().__init__(_t, **kwargs)
+
+	test_object = TestSubClass("this is some test data")
+	
+	test_object_json = test_object.to_json()
+
+	test_object_round_trip = TestSubClass.from_json(test_object_json)
+
+	assert test_object == test_object_round_trip
+	assert test_object.some_other_data == "this is some test data"
+	assert test_object._t == "BH.oM.Base.CustomObject"
