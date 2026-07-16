7eabb79acfd6ae2321fbf3d3496e82c2c4f6719c
Felix Mallinder <felix.mallinder@burohappold.com>
2026-07-16 11:03:05 +0100

    shuffled order of events; ensure we only migrate if the main logs folder exists now

--- Python_Engine/Python/src/python_toolkit/bhom/__init__.py
+++ Python_Engine/Python/src/python_toolkit/bhom/__init__.py
@@ -7,6 +7,7 @@ import tempfile
 import importlib.metadata
 
 BHOM_LOG_FOLDER = Path(path.expandvars("%PROGRAMDATA%/BHoM/Logs"))
+TEMP_LOG_FOLDER = Path(tempfile.gettempdir()) / "BHoM" / "Logs"
 TOOLKIT_NAME = "Python_Toolkit"
 BHOM_VERSION = importlib.metadata.version("python_toolkit")
 
@@ -21,13 +22,14 @@ if not BHOM_LOG_FOLDER.exists():
 
     try:
         BHOM_LOG_FOLDER.mkdir(exist_ok=True, parents=True)
+
+        #migration recovery for any logs in the temp folder
+        if TEMP_LOG_FOLDER.exists():
+            for file in TEMP_LOG_FOLDER.glob("*.log"):
+                file.rename(BHOM_LOG_FOLDER / file.name)
+
     except Exception as e:
-        BHOM_LOG_FOLDER = Path(tempfile.gettempdir()) / "BHoM" / "Logs"
+        BHOM_LOG_FOLDER = TEMP_LOG_FOLDER
         BHOM_LOG_FOLDER.mkdir(exist_ok=True, parents=True)
 
 
-#migration recovery for any logs in the temp folder
-old_temp_log = Path(tempfile.gettempdir()) / "BHoM" / "Logs"
-if old_temp_log.exists():
-    for file in old_temp_log.glob("*.log"):
-        file.rename(BHOM_LOG_FOLDER / file.name)
