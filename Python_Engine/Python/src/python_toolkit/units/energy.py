4d8a777ca16c677d51826462237c019359a509c4
Tom Kingstone <tom.kingstone@burohappold.com>
2026-07-23 11:45:56 +0100

    add some unit enums with converters

---
+++ Python_Engine/Python/src/python_toolkit/units/energy.py
@@ -0,0 +1,18 @@
+﻿from enum import Enum
+
+class EnergyUnit(Enum):
+    """Each unit value is a tuple in the format:
+        (display name, lambda to joules, lambda from joules)
+    """
+    BTU = ("British Thermal Unit", lambda btu: btu * 1.05505585262e3, lambda j: j / 1.05505585262e3)
+    J = ("Joule", lambda j: j, lambda j: j)
+    kBTU = ("Kilo British Thermal Unit", lambda kbtu: kbtu * 1.05505585262e6, lambda j: j / 1.05505585262e6)
+    kJ = ("Kilojoule", lambda kj: kj * 1e3, lambda j: j / 1e3)
+    kWh = ("kilowatt Hour", lambda kwh: kwh * 3.6e6, lambda j: j / 3.6e6)
+    MBTU = ("Mega British Thermal Unit", lambda mbtu: mbtu * 1.05505585262e9, lambda j: j / 1.05505585262e9)
+    MJ = ("Megajoule", lambda mj: mj * 1e6, lambda j: j / 1e6)
+    MWh = ("Megawatt Hour", lambda mwh: mwh * 3.6e9, lambda j: j / 3.6e9)
+    Wh = ("Watt Hour", lambda wh: wh * 3.6e3, lambda j: j / 3.6e3)
+
+    def convert(self, value: float, to_unit: "EnergyUnit"):
+        return to_unit.value[2](self.value[1](value))
---
+++ Python_Engine/Python/src/python_toolkit/units/speed.py
@@ -0,0 +1,13 @@
+﻿from enum import Enum
+
+class SpeedUnit(Enum):
+    """Each unit value is a tuple in the format:
+        (display name, lambda to metres per secomd, lambda from metres per second)
+    """
+    m_s = ("Metres Per Second", lambda m_s: m_s, lambda m_s: m_s)
+    km_h = ("Kilometres Per Hour", lambda km_h: km_h * 3.6e6, lambda m_s: m_s / 3.6e6)
+    mi_h = ("Miles Per Hour", lambda mi_h: mi_h * 5.7936384e6, lambda m_s: m_s / 5.7936384e6)
+    ft_s = ("Feet Per Second", lambda ft_s: ft_s * 3.048e-1, lambda m_s: m_s / 3.048e-1)
+
+    def convert(self, value: float, to_unit: "SpeedUnit"):
+        return to_unit.value[2](self.value[1](value))
