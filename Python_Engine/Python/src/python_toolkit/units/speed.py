448e2087cf5dfd49ddb2d41aa1b76a3fe52e42bf
Tom Kingstone <tom.kingstone@burohappold.com>
2026-07-24 15:13:14 +0100

    minor order of operations wrangling

--- Python_Engine/Python/src/python_toolkit/units/speed.py
+++ Python_Engine/Python/src/python_toolkit/units/speed.py
@@ -5,8 +5,8 @@ class SpeedUnit(Enum):
         (display name, lambda to metres per secomd, lambda from metres per second)
     """
     m_s = ("Metres Per Second", lambda m_s: m_s, lambda m_s: m_s)
-    km_h = ("Kilometres Per Hour", lambda km_h: (km_h * 1e3) / 3.6e3, lambda m_s: (m_s / 1e3) * 3.6e3)
-    mi_h = ("Miles Per Hour", lambda mi_h: (mi_h * 1.609344e3) / 3.6e3, lambda m_s: (m_s / 1.609344e3) * 3.6e3)
+    km_h = ("Kilometres Per Hour", lambda km_h: km_h * (1e3 / 3.6e3), lambda m_s: m_s / (1e3 / 3.6e3))
+    mi_h = ("Miles Per Hour", lambda mi_h: mi_h * (1.609344e3 / 3.6e3), lambda m_s: m_s / (1.609344e3 / 3.6e3))
     ft_s = ("Feet Per Second", lambda ft_s: ft_s * 3.048e-1, lambda m_s: m_s / 3.048e-1)
 
     def convert(self, value: float, to_unit: "SpeedUnit"):
