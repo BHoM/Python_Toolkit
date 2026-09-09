8fbed44ea85160ba569001e9193db6f48009668d
Tom Kingstone <tom.kingstone@burohappold.com>
2026-09-09 11:40:43 +0100

    add UpdateBHoMPackages method

---
+++ Python_Engine/Compute/UpdateBHoMPackages.cs
@@ -0,0 +1,65 @@
+﻿using BH.oM.Python;
+using BH.oM.Python.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+
+namespace BH.Engine.Python
+{
+    public static partial class Compute
+    {
+        public static void UpdateBHoMPackages(this string environmentName, PythonVersion version = PythonVersion.Undefined)
+        {
+            //construct the expected PythonEnvironment and pass to UpdateBHoMPackages
+            PythonEnvironment env = new PythonEnvironment()
+            {
+                Name = environmentName,
+            };
+
+            //python version is only used if the environment name is this toolkits name.
+            if (environmentName == Query.ToolkitName())
+            {
+                if (version == PythonVersion.Undefined)
+                {
+                    BH.Engine.Base.Compute.RecordError("The version must be specified when updating BHoM packages for a base python environment. No updates have occurred.");
+                    return;
+                }
+
+                env.Executable = Path.Combine(Query.DirectoryBaseEnvironment(version), "python.exe");
+            }
+            else
+                env.Executable = Query.VirtualEnvironmentExecutable(environmentName);
+
+            UpdateBHoMPackages(env);
+        }
+
+        [Description("Direct method to update BHoM Python editable packages using the PythonEnvironment directly.")]
+        public static void UpdateBHoMPackages(this PythonEnvironment environment)
+        {
+            if (!Query.VirtualEnvironmentExists(environment.Name))
+            {
+                BH.Engine.Base.Compute.RecordError("Given environment does not exist.");
+                return;
+            }
+
+            string localPackageDirectory = ResolvePackageDirectory(environment);
+
+            if (!Directory.Exists(localPackageDirectory))
+            {
+                BH.Engine.Base.Compute.RecordError($"There is no local package directory for {environment.Name} (searched at \"{localPackageDirectory}\"). No packages were updated.");
+                return;
+            }
+
+            InstallPackageLocal(environment, localPackageDirectory);
+        }
+
+        private static string ResolvePackageDirectory(PythonEnvironment environment)
+        {
+            string packageName = environment.Name;
+            string codeDirectory = Query.DirectoryCode();
+            return Path.Combine(codeDirectory, packageName);
+        }
+    }
+}
